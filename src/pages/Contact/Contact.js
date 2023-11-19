2ce212e6d0713edec6b608076abb0ce272209c21
Deven varu <103780082+Devenvaruv@users.noreply.github.com>
2023-11-18 22:55:35 -0800

    Update Contact.js

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -2,8 +2,21 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <h1>Contact</h1>
-    )
+        <div>
+            <h1>Contact Us</h1>
+            <p>Thank you for visiting our website. If you have any questions, feedback, or need assistance, please feel free to reach out to us. Our team is always ready to help you with your needs.</p>
+            
+            <p>For general inquiries or support:</p>
+            <p>Phone: 99999999</p>
+            <p>Email: <a href="mailto:fake@gmail.com">fake@gmail.com</a></p>
+            
+            <p>If you are interested in our services or need a consultation:</p>
+            <p>Phone: 88888888</p>
+            <p>Email: <a href="mailto:service@example.com">service@example.com</a></p>
+
+            <p>We strive to respond to all inquiries as quickly as possible. Your satisfaction is our priority. We look forward to hearing from you!</p>
+        </div>
+    );
 }
 
 export default Contact;
