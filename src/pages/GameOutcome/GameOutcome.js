2a92ff4dbebf99be2707f853ed4dae5d818fa17b
Satyansh Sharma <satyanshbu@gmail.com>
2023-11-30 14:38:44 -0800

    GameOutcome Added

--- src/pages/GameOutcome/GameOutcome.js
+++ src/pages/GameOutcome/GameOutcome.js
@@ -1,17 +1,39 @@
-import React from 'react';
-// import components like Leaderboard, PlayerCard, etc.
+import React, { useState, useEffect } from 'react';
+
+const playerData = [
+  { id: 1, name: 'Player 1', score: 1500 },
+  { id: 2, name: 'Player 2', score: 1200 },
+  { id: 3, name: 'Player 3', score: 1800 },
+
+];
+
+function Leaderboard({ players }) {
+  return (
+    <div>
+      <h2>Leaderboard</h2>
+      <ul>
+        {players.map((player) => (
+          <li key={player.id}>
+            {player.name}: {player.score} points
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 function GameOutcome() {
-  // Fetch and display game results, total profits, etc.
-  // Maybe use context or redux to manage state across components
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    setPlayers(playerData);
+  }, []);
 
   return (
     <div>
-        <h1>fffff</h1>
-      {/* Leaderboard */}
-      {/* Player cards with profits and costs */}
-      {/* Recap of property details */}
-      {/* Replay or challenge friends */}
+      <h1>Game Outcome</h1>
+      <Leaderboard players={players} />
+      {/* Other components like Player cards, property details, etc. */}
     </div>
   );
 }
