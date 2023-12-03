eba32c28b49a65e044e8a199c145754e1ffaf740
Satyansh Sharma <satyanshbu@gmail.com>
2023-12-02 23:20:03 -0800

    Home js updated

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,41 +1,58 @@
 import React from "react";
 
+/**
+ * Home component representing the introduction and overview of the Austin Real Estate Simulator.
+ */
 const Home = () => {
     return (
         <div className="austin-intro">
-          <h1>Welcome to the Austin Real Estate Simulator!</h1>
-          <h2>Explore the Thriving City of Austin</h2>
-          <p>
-            Once a quiet college town, Austin, Texas, has rapidly grown into a booming tech hub 
-            and metropolis. This transformation has brought a fusion of cultures, a lively music 
-            scene, and innovative businesses, making Austin a magnet for both tourists and new 
-            residents.
-          </p>
-          
-          <h3>The Rise of a Real Estate Powerhouse</h3>
-          <p>
-            With the advent of platforms like Airbnb, Austin has become a hotspot for property 
-            investment. The city's diverse neighborhoods offer unique investment opportunities, 
-            each with its distinct character and potential for varying returns on investment.
-          </p>
-    
-          <h4>Your Journey in Real Estate Investment</h4>
-          <p>
-            Step into the world of real estate investment in Austin. This game challenges you to 
-            maximize profits through smart property acquisitions and effective pricing strategies. 
-            It's more than a game; it's a simulation of real-world investment decisions and market 
-            dynamics.
-          </p>
-    
-          <h2>Become a Part of Austin's Growth Story</h2>
-          <p>
-            As you play, you'll not only develop your skills as a property investor but also gain 
-            insights into the evolving landscape of Austin's real estate market. Join us in this 
-            exciting and educational journey as you carve your path to becoming a successful real 
-            estate mogul in one of America's most dynamic cities.
-          </p>
+            {/* Main Heading */}
+            <h1>Welcome to the Austin Real Estate Simulator!</h1>
+
+            {/* Subheading */}
+            <h2>Explore the Thriving City of Austin</h2>
+
+            {/* Introduction Paragraph */}
+            <p>
+                Once a quiet college town, Austin, Texas, has rapidly grown into a booming tech hub
+                and metropolis. This transformation has brought a fusion of cultures, a lively music
+                scene, and innovative businesses, making Austin a magnet for both tourists and new
+                residents.
+            </p>
+
+            {/* Section Heading */}
+            <h3>The Rise of a Real Estate Powerhouse</h3>
+
+            {/* Real Estate Powerhouse Paragraph */}
+            <p>
+                With the advent of platforms like Airbnb, Austin has become a hotspot for property
+                investment. The city's diverse neighborhoods offer unique investment opportunities,
+                each with its distinct character and potential for varying returns on investment.
+            </p>
+
+            {/* Subsection Heading */}
+            <h4>Your Journey in Real Estate Investment</h4>
+
+            {/* Real Estate Investment Journey Paragraph */}
+            <p>
+                Step into the world of real estate investment in Austin. This game challenges you to
+                maximize profits through smart property acquisitions and effective pricing strategies.
+                It's more than a game; it's a simulation of real-world investment decisions and market
+                dynamics.
+            </p>
+
+            {/* Closing Subheading */}
+            <h2>Become a Part of Austin's Growth Story</h2>
+
+            {/* Closing Paragraph */}
+            <p>
+                As you play, you'll not only develop your skills as a property investor but also gain
+                insights into the evolving landscape of Austin's real estate market. Join us in this
+                exciting and educational journey as you carve your path to becoming a successful real
+                estate mogul in one of America's most dynamic cities.
+            </p>
         </div>
-      );
+    );
 }
 
 export default Home;
